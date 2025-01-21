00431331796e0ba24ffeb95e3355f1d6f3f0e328
Sam Joshua P <samjoshuacs2002@gmail.com>
2025-01-21 21:21:21 +0530

    refactor: remove unused imports in Cart component

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { Minus, Plus, ShoppingBag, Trash2 } from "lucide-react";
+import {  ShoppingBag, Trash2 } from "lucide-react";
 import { BASE_URL } from "../App";
 
 const Cart = () => {
